33b76e90fa1ad58565dd80ce21d6224f77af1faf
harikrishnanmkd <116252215+harikrishnanmkd@users.noreply.github.com>
2026-07-15 12:00:54 +0530

    update forms and taskpage

--- tasks/forms.py
+++ tasks/forms.py
@@ -6,3 +6,17 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['title'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Enter task title'
+        })
+
+        self.fields['description'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Enter task description',
+            'rows': 4
+        })
